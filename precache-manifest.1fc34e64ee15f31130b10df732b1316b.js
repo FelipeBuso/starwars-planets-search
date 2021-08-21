f52a1641a43fbdac97a9e018d01fd71d99dc78ee
Felipe Buso <felipe_buso@hotmail.com>
2021-08-21 19:11:05 -0300

    Updates

---
+++ precache-manifest.1fc34e64ee15f31130b10df732b1316b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3b28b43f0787a71420349222afeec50",
+    "url": "/starwars-planets-search/index.html"
+  },
+  {
+    "revision": "2a81de39988e7a71e078",
+    "url": "/starwars-planets-search/static/css/main.c94e52f2.chunk.css"
+  },
+  {
+    "revision": "06535bae2f287a70fcf3",
+    "url": "/starwars-planets-search/static/js/2.fdf63519.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/starwars-planets-search/static/js/2.fdf63519.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a81de39988e7a71e078",
+    "url": "/starwars-planets-search/static/js/main.6586e3f7.chunk.js"
+  },
+  {
+    "revision": "1b9e9e3eb9707d3157b0",
+    "url": "/starwars-planets-search/static/js/runtime-main.d5e8347d.js"
+  },
+  {
+    "revision": "0202a998125a13ed541ccd1f089d3d6f",
+    "url": "/starwars-planets-search/static/media/star-wars-logo.0202a998.png"
+  }
+]);
